9bd8e2b4ca05b638aecbc14270582d06f1b814e2
sam <samdatascience2024@gmail.com>
2026-03-08 18:02:18 +0530

    feat-basic1

---
+++ basic1.py
@@ -0,0 +1,4 @@
+print("HEllow world have to cheer it up .....")
+print("HEllow world have to cheer it up 11111.....")
+print("HEllow world have to cheer it up 22222.....")
+print("HEllow world have to cheer it up 33333.....")
